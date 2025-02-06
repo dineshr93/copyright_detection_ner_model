592116b004be4eb28316eff9797a9ce37a0b86c8
Dinesh <Dinesh.Ravi@elektrobit.com>
2025-02-06 14:54:50 +0100

    adjust the devcontainer

--- .devcontainer/container/devcontainer.sh
+++ .devcontainer/container/devcontainer.sh
@@ -1,6 +1,6 @@
 {
 	"name": "Python 3 NER Copyright texts detection Pipeline",
 	"image": "mcr.microsoft.com/devcontainers/python:1-3.10-bookworm",
-	"postCreateCommand": "/bin/bash -c 'bash .devcontainer/setup.sh'",
-	"postStartCommand": "pip install --upgrade pip setuptools wheel && pip cache purge && pip install -r requirements.txt || pip cache purge && pip install -r requirements.txt"
+	"postCreateCommand": "/bin/bash -c 'bash .devcontainer/setup.sh && pip install --upgrade pip setuptools wheel && pip cache purge && pip install -r requirements.txt || pip cache purge && pip install -r requirements.txt'",
+	# "postStartCommand": "pip install --upgrade pip setuptools wheel && pip cache purge && pip install -r requirements.txt || pip cache purge && pip install -r requirements.txt"
 }
